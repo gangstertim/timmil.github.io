41aec742691f3776a61694ceaa8913ec2b482410
Tim <tim@hangify.com>
2016-01-05 00:48:03 -0500

    Adds some chords

--- js.js
+++ js.js
@@ -1,10 +1,12 @@
 var myElement = document.getElementById('myElement');
 
-// create a simple instance
 // by default, it only adds horizontal recognizers
 var mc = new Hammer(myElement);
+var cChord = new Audio('C.mp3');
+var ebChord = newAudio('Eb.mp3');
 
 // listen to events...
 mc.on("panleft panright tap press", function(ev) {
     myElement.textContent = ev.type +" gesture detected.";
+    cChord.play();
 });
