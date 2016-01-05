3de560e6796b58a806a936ecc6e251d5327abdff
Tim <tim@hangify.com>
2016-01-05 01:54:58 -0500

    Last attempt to get ios to play html audio

--- js.js
+++ js.js
@@ -5,27 +5,22 @@ var cButton = document.getElementById('c');
 
 // by default, it only adds horizontal recognizers
 var mc = new Hammer(strings);
+var currentChord, aChord, bChord, cChord;
 var aListener = new Hammer(aButton);
 var bListener = new Hammer(bButton);
 var cListener = new Hammer(cButton);
 
-var aChord = new Audio('chords/1.wav');
-var bChord = new Audio('chords/2.wav');
-var cChord = new Audio('chords/3.wav');
-
-var currentChord = aChord;
-
-strings.addEventListener('touchmove', function() {
-  currentChord.play();
-});
+//strings.addEventListener('touchmove', function() {
+//  currentChord.play();
+//});
 // listen to events...
-/*
+
 mc.on("panleft panright", function(e) {
     strings.textContent = e.type +" gesture detected.";
     currentChord.pause();
     currentChord.currentTime = 0;
     currentChord.play();
-});*/
+});
 
 mc.on("tap press", function(e) {
    strings.textContent = e.type;
@@ -34,13 +29,22 @@ mc.on("tap press", function(e) {
 });
 
 aListener.on("tap press", function(e) {
+   if (!aChord) {
+     aChord =  new Audio('chords/1.wav');
+   }
    currentChord = aChord;
 });
 
 bListener.on("tap press", function(e) {
-  currentChord = bChord;
+   if (!bChord) {
+     bChord =  new Audio('chords/2.wav');
+   }
+   currentChord = bChord;
 });
 
 cListener.on("tap press", function(e) {
+  if (!cChord) {
+    cChord =  new Audio('chords/3.wav');
+  }
   currentChord = cChord;
 });
