d5d7d48457bbeecaf7cc96c20cf7233e9cd09147
Tim <tim@hangify.com>
2016-01-05 00:48:47 -0500

    Fixes typo

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ var myElement = document.getElementById('myElement');
 // by default, it only adds horizontal recognizers
 var mc = new Hammer(myElement);
 var cChord = new Audio('C.mp3');
-var ebChord = newAudio('Eb.mp3');
+var ebChord = new Audio('Eb.mp3');
 
 // listen to events...
 mc.on("panleft panright tap press", function(ev) {
